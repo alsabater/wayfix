02b39381592ef337332d074f4d0e3bcc54a0d711
alsabater <alvaro@sabater.eu>
2016-01-20 12:13:23 +0100

    Added sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			session[:user_id] = user.id
+			redirect_to root_path
+		else
+			flash.now[:danger] = "El email o el password no se corresponden con ningún usuario"
+			render 'new'
+		end
+	end
+
+	def destroy
+
+	end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,24 @@
 class UsersController < ApplicationController
+	
 	def index
 	end
 
 	def new
 		@user = User.new
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			flash[:success]="User successfully created"
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:name, :surname_1, :surname_2, :dni, :passport, :genre, :birthday, :country, :phone_number_1, :phone_number_2, :email, :password, :job_position)
 	end
 end
