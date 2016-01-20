10fe19efbc70a9bc5e52805349fc74e24e4f4329
alsabater <alvaro@sabater.eu>
2016-01-20 22:21:15 +0100

    Fixing error Heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
 		if @user.save
 			flash[:success]="User successfully created"
 			session[:user_id] = @user.id
-			redirect_to user_path
+			redirect_to user_path(@user.id)
 		else
 			render 'new'
 		end
