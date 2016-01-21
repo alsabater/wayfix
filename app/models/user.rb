36d81ffa769e60baa38a437418b4ffc88e249f61
alsabater <alvaro@sabater.eu>
2016-01-21 19:11:19 +0100

    No password required to update users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
 	has_many :centers, through: :centerandusers
 
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :name, :surname_1, :email, :password, presence: true
+	validates :name, :surname_1, presence: true
+	validates :email, :password, presence: true, on: :create
 	validates :email, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
 	
 	has_secure_password
